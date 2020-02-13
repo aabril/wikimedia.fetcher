65c3a83f49159b9d790a3b126503e223bad0179b
Albert Abril <albert@macbook>
2020-02-13 17:09:21 +0000

    languages in a different file (TBC)

--- src/scripts.js
+++ src/scripts.js
@@ -1,26 +1,3 @@
-const languages = {
-    "en": {
-        "en": "English",
-        "es": "Spanish",
-        "cat": "Catalan",
-        "eo": "Esperanto",
-        "zh": "Chinese",
-        "ja": "Japanese",
-        "ru": "Russian",
-        "ar": "Arabic"
-    },
-    "es": {
-        "en": "Inglés",
-        "es": "Español",
-        "cat": "Catalán",
-        "eo": "Esperanto",
-        "zh": "Chino",
-        "ja": "Japonés",
-        "ru": "Ruso",
-        "ar": "Árabe"
-    }
-}
-
 class appComponent {
     constructor() {
         let html = this.html()
